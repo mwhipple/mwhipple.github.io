a67cf4a82e1d1e3072f1c6646d8bf8779fbf1e02
Matt <mattwhipple@acm.org>
2024-02-19 19:17:19 -0500

    Fix process bug

--- remove_comment_prefix.c
+++ remove_comment_prefix.c
@@ -1,15 +1,17 @@
 /**
- *
+ * There's a bug somewhere in here that leads to some output getting mangled...
+ * but where that could be popping up is unclear.
  */
 
 #include <stdio.h>
 
 int main(int _argc, char * _argv) {
 	char * line = NULL;
+	int prefix_len;
 	size_t len;
 
 	while ((getline(&line, &len, stdin) != -1)) {
-		for (;*line == '#'; line++);
-		printf("%s", line);
+		for (prefix_len=0 ;line[prefix_len] == '#'; prefix_len++);
+		printf("%s", line+prefix_len);
 	}
 }
